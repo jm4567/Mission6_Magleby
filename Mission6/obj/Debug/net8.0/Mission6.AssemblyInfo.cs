04f6d31befbfda22fb1f93ae00765e85c8783d21
jm4567 <jenna.magleby@gmail.com>
2025-02-18 08:46:31 -0700

    commit

--- Mission6/obj/Debug/net8.0/Mission6.AssemblyInfo.cs
+++ Mission6/obj/Debug/net8.0/Mission6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd05568e084c2345b28c26207145f817f6784da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9015b3a96adb6f55fc63cd1d97ae4d3c96566d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
